c503c3919cfbc8ca63dc8690e64b74edd1acec4e
Peter5793 <masaba.peter68@gmail.com>
2020-11-26 22:04:02 +0300

    add prototypes to holberton.h

--- holberton.h
+++ holberton.h
@@ -1,30 +1,22 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include<stdio.h> 
-#include<string.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<sys/wait.h> 
-#include<readline/readline.h> 
-#include<readline/history.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <readline/history.h>
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
 
+// Clearing the shell using escape sequences
+#define clear() printf("\033[H\033[J") 
 /** 
  *make a function that would initialize shell program
  *write down an algorithm
  *we a struct that allows us create commands
+
  */
-
-typedef struct existing
-{
-  char *command;
-  int (*f)(char **argv);
-}existing_t;
-
-/*function prototypes*/
-{
-  char *read_input ();
-  int simple_addition(int *f);
-  (void) execute ();
-  
-}
+int input(char *str);
+#endif
